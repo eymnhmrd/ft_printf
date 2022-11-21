df546e58f12cafc9af62ebc7863baf97519fdf65
Aymane Hamrad <ahamrad@e2r7p13.1337.ma>
2022-11-21 02:10:55 +0100

    done v2

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,34 @@
 /*   By: ahamrad <ahamrad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 00:19:42 by ahamrad           #+#    #+#             */
-/*   Updated: 2022/11/21 00:00:42 by ahamrad          ###   ########.fr       */
+/*   Updated: 2022/11/21 02:09:41 by ahamrad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void	ft_check_percent(va_list list, char c, int *count)
+{
+	if (c == 'c')
+		ft_putchar(va_arg(list, int), count);
+	if (c == 's')
+		ft_putstr(va_arg(list, char *), count);
+	if (c == 'd' || c == 'i')
+		ft_putnbr(va_arg(list, int), count);
+	if (c == 'u')
+		ft_putunsigned(va_arg(list, unsigned int), count);
+	if (c == '%')
+		ft_putchar('%', count);
+	if (c == 'x')
+		ft_puthexa_x(va_arg(list, unsigned int), count);
+	if (c == 'X')
+		ft_puthexa_xmaj(va_arg(list, unsigned int), count);
+	if (c == 'p')
+	{
+		ft_putstr("0x", count);
+		ft_puthexa_x(va_arg(list, unsigned long), count);
+	}
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -26,25 +49,7 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
-				ft_putchar(va_arg(list, int), &count);
-			if (format[i] == 's')
-				ft_putstr(va_arg(list, char *), &count);
-			if (format[i] == 'd' || format[i] == 'i')
-				ft_putnbr(va_arg(list, int), &count);
-			if (format[i] == 'u')
-				ft_putunsigned(va_arg(list, unsigned int), &count);
-			if (format[i] == '%')
-				ft_putchar('%', &count);
-			if (format[i] == 'x')
-				ft_puthexa_x(va_arg(list, unsigned int), &count);
-			if (format[i] == 'X')
-				ft_puthexa_xmaj(va_arg(list, unsigned int), &count);
-			if (format[i] == 'p')
-			{
-				ft_putstr("0x", &count);
-				ft_puthexa_x(va_arg(list, unsigned long), &count);
-			}
+			ft_check_percent(list, format[i], &count);
 		}
 		else
 		{
